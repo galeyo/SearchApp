f40075a2bd1b8ac75379037d8fecfd0c7567cd51
galeyo <nickokhapkin@gmail.com>
2020-11-18 08:56:58 +0100

    fix

---
+++ Application/Comments/CommentMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Comments
+{
+    public class CommentMapping : Profile
+    {
+        public CommentMapping()
+        {
+            CreateMap<Comment, CommentDto>()
+                .ForMember(d => d.Username, opt => opt.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Author.Image));
+        }
+    }
+}
